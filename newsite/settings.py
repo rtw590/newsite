01342aa3aa355769e875003fead6134a72b170c2
Rob <32942467+rtw590@users.noreply.github.com>
2017-12-06 17:22:07 -0600

    added sharing and comments

--- newsite/settings.py
+++ newsite/settings.py
@@ -101,3 +101,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'my_account@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
